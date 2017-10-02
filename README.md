9905541506321c17bef3d1d6189bd07d5a9dd42f
DELL\gluce <liucheng@tangi0.com>
2017-10-02 16:38:45 +0100

    update readme and iptable.sh

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-"# iptable" 
+"# iptable for oversea Chinese who want to visit Chinese website" 
+you need to enable openvpn service or L2TP/IPSec service in your server
--- iptables.sh
+++ iptables.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 out_iface="eth0"
-server_IP="1.2.3.4"
-subnet="192.168.42.0/24"
+server_IP="1.2.3.4" # server address, for AWS EC2 user, it is the internal net address
+subnet="192.168.42.0/24" # internal net addresses
  
 # strongswan
-iptables -A INPUT -p udp --dport 500 -j ACCEPT
-iptables -A INPUT -p udp --dport 4500 -j ACCEPT
+# iptables -A INPUT -p udp --dport 500 -j ACCEPT
+# iptables -A INPUT -p udp --dport 4500 -j ACCEPT
+
+# openvpn
+iptables -A INPUT -p udp --dport 1194 -j ACCEPT
  
-# Moved to /etc/sysctl.conf: net.ipv4.ip_forward=1
-# 打开网卡转发功能。要把客户端的请求转发到公网网卡
+# remember to enable IP forward first
 # echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -s ${subnet} -o ${out_iface} -j MASQUERADE
-# 允许转发
+# allow forward to subnet
 iptables -A FORWARD -s ${subnet} -j ACCEPT
  
 [ -r chnroute.txt ] || curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > chnroute.txt
@@ -21,7 +23,7 @@ iptables -t nat -N SHADOWSOCKS
  
 iptables -t nat -A SHADOWSOCKS -d $server_IP -j RETURN
  
-# 内网网段
+# internel net, pass
 iptables -t nat -A SHADOWSOCKS -d 0.0.0.0/8 -j RETURN
 iptables -t nat -A SHADOWSOCKS -d 10.0.0.0/8 -j RETURN
 iptables -t nat -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN
@@ -31,17 +33,17 @@ iptables -t nat -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN
 iptables -t nat -A SHADOWSOCKS -d 224.0.0.0/4 -j RETURN
 iptables -t nat -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
  
-# 创建ipset列表
-#ipset -exist create gfwlist hash:net
+# build ipset table
+# ipset -exist create gfwlist hash:net
 ipset -exist create chnroute hash:net
 cat chnroute.txt | sudo xargs -I ip ipset -exist add chnroute ip
  
-# gfwlist 走ss转发
+# gfwlist forward
 #iptables -t nat -A SHADOWSOCKS -m set --match-set gfwlist dst -p tcp  -j REDIRECT --to-ports 1080
  
-# 国内ip表直连
+# Chinese IP forward 
 iptables -t nat -A SHADOWSOCKS -m set --match-set chnroute dst -p tcp -j REDIRECT --to-ports 1080
-# 其他连接走ss转发
+# all others pass
 iptables -t nat -A SHADOWSOCKS -p tcp  -j RETURN
  
 iptables -t nat -A PREROUTING -s ${subnet} -p tcp -j SHADOWSOCKS
